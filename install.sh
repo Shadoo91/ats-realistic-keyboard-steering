2b08bf06f0a13e1ce1b0587c87c73c6e8ecbe9d0
Shadoo91 <fbender@tutamail.com>
2026-05-13 11:45:27 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "==============================================================================="
-echo "   ATS Realistic-Keayboard-Steering (RKS) (Turbo-Mode) for Linux ~ by Shadoo   "
-echo "==============================================================================="
+echo "======================================================================================"
+echo "   ATS Realistic-Keayboard-Steering (RKS) (Turbo-Mode) - for Windows ~ by Shadoo91    "
+echo "======================================================================================"
 echo
 
 TARGET_DIR="$HOME/.steam/steam/steamapps/compatdata/270880/pfx/drive_c/users/steamuser/Documents/American Truck Simulator/profiles"
@@ -28,24 +28,4 @@ find "$TARGET_DIR" -name "controls.sii" | while read -r FILE; do
     
     chmod 644 "$FILE"
     
-    sed -i 's|config_lines\[330\]:.*|config_lines\[330\]: "mix dsteerleft `keyboard.a?0`"|' "$FILE"
-    sed -i 's|config_lines\[331\]:.*|config_lines\[331\]: "mix dsteerright `keyboard.d?0`"|' "$FILE"
-    sed -i 's|config_lines\[332\]:.*|config_lines\[332\]: "mix dsteering `(keyboard.a?0 - keyboard.d?0) * (0.35 + keyboard.space?0 * 0.65)`"|' "$FILE"
-    sed -i 's|config_lines\[333\]:.*|config_lines\[333\]: "mix steering `dsteering`"|' "$FILE"
-    sed -i 's|config_lines\[334\]:.*|config_lines\[334\]: "mix msteering `-mouse.rel_position.x?0 * c_msens`"|' "$FILE"
-    sed -i 's|config_lines\[335\]:.*|config_lines\[335\]: "mix mpedals `-mouse.rel_position.y?0 * c_msens`"|' "$FILE"
-    sed -i 's|config_lines\[336\]:.*|config_lines\[336\]: "mix dforward `0`"|' "$FILE"
-    sed -i 's|config_lines\[337\]:.*|config_lines\[337\]: "mix dbackward `0`"|' "$FILE"
-    sed -i 's|config_lines\[338\]:.*|config_lines\[338\]: "mix aforward `(keyboard.w?0 * 0.35) + (keyboard.lalt?0 * 0.55)`"|' "$FILE"
-    sed -i 's|config_lines\[339\]:.*|config_lines\[339\]: "mix abackward `keyboard.s?0 * (0.10 + keyboard.space?0 * 0.50)`"|' "$FILE"
-    sed -i 's|config_lines\[340\]:.*|config_lines\[340\]: "mix forward `aforward`"|' "$FILE"
-    sed -i 's|config_lines\[341\]:.*|config_lines\[341\]: "mix backward `abackward`"|' "$FILE"
-    
-    sed -i 's|config_lines\[358\]:.*|config_lines\[358\]: "mix airhorn `keyboard.lalt?0 \| semantical.airhorn?0`"|' "$FILE"
-    sed -i 's|mix parkingbrake.*|mix parkingbrake `keyboard.backspace?0`"|' "$FILE"
-    
-    chmod 444 "$FILE"
-done
-
-echo
-echo "[INFO] Installation completed successfully!"
+    sed -i 's|config_lines\[330\]:.*|config_lines\[330\]: "mix dsteerleft `keyboard.a?0`"|'
