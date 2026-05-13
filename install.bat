8e4a5b09d3436b2cb754854f00d30a2e707cf3f9
Shadoo91 <fbender@tutamail.com>
2026-05-13 12:46:39 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
 @echo off
 title ATS Turbo-Input Installer
 echo Launching Installer...
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('githubusercontent.com')"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://github.com/Shadoo91/ats-realistic-keyboard-steering')"
 if %errorlevel% neq 0 (
     echo [ERROR] Script failed to launch! Make sure you are connected to the internet.
     pause
